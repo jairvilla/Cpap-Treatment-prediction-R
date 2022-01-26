1aee03d4bd55f9444ec256bd6a867f5da23c6d7a
jairvilla <jair.villanueva94@uac.edu.co>
2022-01-26 13:16:36 -0500

    preprocesado_22

---
+++ prepocessing22.R
@@ -0,0 +1,12 @@
+
+# Project: Cpap Project
+# Date: 26-Jan-22
+# Author: Jair Villanueva 
+# Aim: Data pre-processing
+
+# Change names
+datacpap<-as.data.frame(cpapfinal)
+
+
+dim(cpapfinal)
+names(cpapfinal)
